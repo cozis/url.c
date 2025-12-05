bc5679aa11a825b7556c6d35a602647914caea18
Francesco Cozzuto <francesco.cozzuto@gmail.com>
2025-12-06 00:53:53 +0100

    Add examples

---
+++ build.bat
@@ -0,0 +1,2 @@
+gcc url.c examples/reference.c -o reference.exe -Wall -Wextra -ggdb
+gcc url.c examples/parse.c -o parse.exe -Wall -Wextra -ggdb
---
+++ build.sh
@@ -0,0 +1,2 @@
+gcc url.c examples/reference.c -o reference.out -Wall -Wextra -ggdb
+gcc url.c examples/parse.c -o parse.out -Wall -Wextra -ggdb
