841f0731e953b70ea3c79fe98dfce3cffdfb6ad3
Francesco Cozzuto <francesco.cozzuto@gmail.com>
2025-12-06 01:33:50 +0100

    Add example for url_decode_field

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-gcc url.c examples/reference.c -o reference.exe -Wall -Wextra -ggdb
-gcc url.c examples/parse.c -o parse.exe -Wall -Wextra -ggdb
+gcc url.c examples/reference.c    -o reference.exe    -Wall -Wextra -ggdb
+gcc url.c examples/parse.c        -o parse.exe        -Wall -Wextra -ggdb
+gcc url.c examples/decode_field.c -o decode_field.exe -Wall -Wextra -ggdb
--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-gcc url.c examples/reference.c -o reference.out -Wall -Wextra -ggdb
-gcc url.c examples/parse.c -o parse.out -Wall -Wextra -ggdb
+gcc url.c examples/reference.c    -o reference.out    -Wall -Wextra -ggdb
+gcc url.c examples/parse.c        -o parse.out        -Wall -Wextra -ggdb
+gcc url.c examples/decode_field.c -o decode_field.out -Wall -Wextra -ggdb
